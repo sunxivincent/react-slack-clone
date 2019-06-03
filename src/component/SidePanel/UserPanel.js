5a9f21435f1a8489f95bcecd84639536f16cd068
xi <xisun1990@gmail.com>
2019-06-03 01:25:30 -0700

    add dropdown in user panel

---
+++ src/component/SidePanel/UserPanel.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { Grid, Header, Icon, Dropdown} from 'semantic-ui-react';
+
+class UserPanel extends React.Component {
+  render() {
+    return (
+      <Grid style={{ background: '#4c3c4c' }}>
+        <Grid.Column>
+          <Grid.Row style={{ padding: '1.2em', margin:0 }}>
+            <Header inverted floated="left" as="h2">
+              <Icon name="code"/>
+              <Header.Content>Dev chat</Header.Content>
+            </Header>
+          </Grid.Row>
+
+          <Header styled={{ padding: "0.25em" }} as="h4" inverted>
+            <Dropdown trigger={
+              <span>User</span>
+            } options={this.dropdownOptions()}/>
+          </Header>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+
+  dropdownOptions = () => [
+    {
+      key: 'user',
+      text: <span>Signed in as <strong>User</strong> </span>,
+      disabled: true
+    },
+    {
+      key: 'avatar',
+      text: <span>Change Avatar</span>,
+    },
+    {
+      key: 'signout',
+      text: <span>Sign Out</span>,
+    }
+  ]
+}
+
+export default UserPanel;
