c3fa530833022bac0ef82331ef344cb7c5cc3c89
xi <xisun1990@gmail.com>
2019-06-03 00:52:28 -0700

    add four mock panel components

--- src/component/App.js
+++ src/component/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Grid } from 'semantic-ui-react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        APP
-      </div>
-    );
-  }
-}
+import './App.css';
+import ColorPanel from "./ColorPanel/ColorPanel";
+import SidePanel from "./SidePanel/SidePanel";
+import Messages from "./Messages/Messages";
+import MetaPanel from "./MetaPanel/MetaPanel";
+
+const App = () => (
+  <Grid columns="equal" className="app" style={{ background: '#eee' }} >
+    <ColorPanel />
+    <SidePanel />
+    <Grid.Column style={{ marginLeft: 320}}>
+      <Messages />
+    </Grid.Column>
+    <Grid.Column width={4}>
+      <MetaPanel />
+    </Grid.Column>
+  </Grid>
+);
 
 export default App;
